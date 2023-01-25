2255a885c18c7d807126d35b80a8d01b342061f5
EyaadM <eyaadmir@gmail.com>
2023-01-25 13:42:28 -0800

    Fixed up ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,18 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseLargerArray() {
+    int[] input1 = {3, 4, 5};
+    assertArrayEquals(new int[]{5, 4, 3}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseLargerArrayInPlace() {
+    int[] input1 = {3, 4, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 4, 3}, input1);
   }
 }
